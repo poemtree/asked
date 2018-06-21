78441769b29411a0f4170f38b5d91c5e7db903bf
poemtree <yym1232@gmail.com>
2018-06-21 02:15:20 +0000

    회원가입 추가

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
 
   delete 'posts/:id' => 'posts#destroy'
 
+  get 'signup' => 'users#create'
+
+  get 'users' => 'users#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
